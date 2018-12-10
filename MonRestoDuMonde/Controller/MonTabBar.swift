b24ffaf90b3738baa7ffc6f3f6bb0c090f5bae25
theomarie <theodu14160@hotmail.fr>
2018-12-10 19:00:07 +0100

    tabBarCustom

---
+++ MonRestoDuMonde/Controller/MonTabBar.swift
@@ -0,0 +1,30 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Theo Marie on 10/12/2018.
+//  Copyright © 2018 Theo Marie. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MonRestoDuMonde/Utilitaire/Constantes.swift
@@ -0,0 +1,9 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Theo Marie on 10/12/2018.
+//  Copyright © 2018 Theo Marie. All rights reserved.
+//
+
+import Foundation
